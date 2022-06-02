a6f36a48abad8044734f473de05a4cfa029220f6
ashischettri <chettri.ashis@webexpertsnepal.com>
2022-06-02 22:02:44 +0545

    Implemented morgan, cors and json body parser

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 
-// Start express application
+// Initialized and start express application
 const app = express();
+
+// Implement CORS
+app.use(cors());
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '20kb'
+  })
+);
+
+app.all('*', (req, res, next) => {
+  next(new Error(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
