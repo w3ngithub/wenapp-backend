7c84a366f826bf0cb705275aaf17318f33acf69d
ashischettri <chettri.ashis@webexpertsnepal.com>
2022-06-02 17:22:47 +0545

    Added comments to functions

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-// handling synchronous exception errors
+// Handle uncaught exception of application and exit application
 process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
+// Load config variables
 dotenv.config({ path: './config.env' });
-
 const app = require('./app');
 
+// Replace db password stored in config file
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
+// Connect mongo db using mongoose client
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+    useNewUrlParser: true
   })
   .then(() => console.log('DB connection successful!'));
 
+// Start express node application
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-// handling asynchronous promise rejection error
+// Handle unhandled promise of application and exit application
 process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
