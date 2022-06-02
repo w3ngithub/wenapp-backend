bd2ed7a570ecaac8e8b0829bf0d89d5ad1ba9399
ashischettri <chettri.ashis@webexpertsnepal.com>
2022-06-02 16:09:15 +0545

    Open express server and connect Mongo DB

---
+++ server.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// handling synchronous exception errors
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection successful!'));
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+// handling asynchronous promise rejection error
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
