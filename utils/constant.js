6282dcd384481fb995a49b3949cf103367bf048e
ashischettri <chettri.ashis@webexpertsnepal.com>
2022-06-10 16:52:47 +0545

    Added  static dropdown key & values as object for  constants

---
+++ utils/constant.js
@@ -0,0 +1,102 @@
+// Defining constants values for dropdowns
+
+exports.userRoles = {
+  admin: 'Admin',
+  lead: 'Lead',
+  manager: 'Project Manager',
+  editor: 'Editor',
+  normal: 'Normal',
+  hr: 'HR',
+  finance: 'Finance'
+};
+
+exports.userPosition = {
+  MT: 'Management',
+  TL: 'Team Lead',
+  PM: 'Project Manager',
+  WD: 'WordPress Developer',
+  FD: 'Frontend Developer',
+  RD: 'React Developer',
+  DO: 'DevOps',
+  QA: 'Quality Assurance',
+  HR: 'Human Resources',
+  FN: 'Finance',
+  TE: 'Trainee',
+  IN: 'Interns'
+};
+
+exports.projectTypes = {
+  CT: 'Contao',
+  CB: 'Custom Build',
+  FX: 'Fixing',
+  MT: 'Maintenance',
+  MG: 'Migration',
+  PG: 'Plugin Development',
+  PT: 'Premium Theme',
+  PD: 'PSD Design',
+  RT: 'React',
+  RV: 'Revamp'
+};
+
+exports.projectStatus = {
+  AR: 'Awaiting Response',
+  CL: 'Completed',
+  NF: 'Need Follow Up',
+  OG: 'On Going',
+  OH: 'On Hold'
+};
+
+exports.clients = {
+  360: '360',
+  BS: 'Brainsell',
+  CF: 'Cyber Family',
+  FS: 'Ferras',
+  FT: 'Fortress',
+  HT: 'Houston',
+  IW: 'ID Waiter',
+  KS: 'Kickstartup',
+  OK: 'Online Karma',
+  RS: 'Rysen',
+  VA: 'Vista Alliance',
+  WS: 'Websults',
+  MS: 'Miscellaneous'
+};
+
+exports.leaveType = {
+  SL: 'Sick Leave',
+  CL: 'Casual Leave',
+  HD: ['First Half', 'Second Half'],
+  PL: 'Paternity Leave',
+  ML: 'Maternity Leave'
+};
+
+exports.logType = {
+  BG: 'Bug',
+  CR: 'Change Request',
+  DE: 'Data Entry',
+  DB: 'Debugging',
+  DN: 'Discussion',
+  FX: 'Fixing',
+  MT: 'Maintenance',
+  MG: 'Migration',
+  NR: 'New Request',
+  QA: 'QA',
+  QF: 'QA Fixing',
+  RS: 'Research',
+  RW: 'Review',
+  RFE: 'RFE',
+  MS: 'Miscellaneous'
+};
+
+exports.noticeCategory = {
+  AN: 'Announcement',
+  EV: 'Event',
+  HD: 'Holiday'
+};
+
+exports.blogCategory = {
+  DG: 'Design',
+  DV: 'Development',
+  GN: 'General',
+  UC: 'Uncategorized'
+};
