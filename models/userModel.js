047e6244699b49ad71529180d9f615111654c057
ashischettri <chettri.ashis@webexpertsnepal.com>
2022-06-11 21:25:10 +0545

    Hashed password -0and removed persist of confirm password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -33,7 +34,6 @@ const userSchema = new mongoose.Schema(
     passwordConfirm: {
       type: String,
       required: [true, 'Please confirm your password'],
-      select: false,
       validate: {
         // This only works on CREATE and SAVE!!!
         validator: function (el) {
@@ -60,11 +60,21 @@ const userSchema = new mongoose.Schema(
   },
   {
     timestamps: true
-  },
-  {
-    versionKey: false
   }
 );
+
+// Document Middleware
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
