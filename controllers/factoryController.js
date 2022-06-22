4453aaaad0c9325955689d49144c804e05de3126
ashischettri <chettri.ashis@webexpertsnepal.com>
2022-06-22 17:59:15 +0545

    Removed populate option and instead added on specific model

--- controllers/factoryController.js
+++ controllers/factoryController.js
@@ -2,12 +2,9 @@ const asyncError = require('../utils/asyncError');
 const AppError = require('../utils/appError');
 const APIFeatures = require('../utils/apiFeatures');
 
-exports.getOne = (Model, popOptions) =>
+exports.getOne = (Model) =>
   asyncError(async (req, res, next) => {
-    let query = Model.findById(req.params.id);
-
-    if (popOptions) query = query.populate(popOptions);
-    const doc = await query;
+    const doc = await Model.findById(req.params.id);
 
     if (!doc) {
       return next(new AppError('No document found with that ID', 404));
