d6b0cc37d3c4d2e798dbeb379a1f5528b3cf83be
ashischettri <chettri.ashis@webexpertsnepal.com>
2022-07-11 17:54:58 +0545

    Used generic setUserId function from auth middleware

--- controllers/blogs/blogController.js
+++ controllers/blogs/blogController.js
@@ -1,12 +1,6 @@
 const Blog = require('../../models/blogs/blogModel');
 const factory = require('../factoryController');
 
-// Setuser id for the blog routes
-exports.setBlogUserIds = (req, res, next) => {
-  if (!req.body.user) req.body.user = req.user.id;
-  next();
-};
-
 exports.getBlog = factory.getOne(Blog);
 exports.getAllBlogs = factory.getAll(Blog);
 exports.createBlog = factory.createOne(Blog);
