43b1b53eeb284a1ec0842b217b334a348be2ad04
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-07-18 17:30:28 +0545

    factory controller added to holidays

--- controllers/holidays/holidaysController.js
+++ controllers/holidays/holidaysController.js
@@ -1,17 +1,8 @@
 const Holiday = require('../../models/holidays/holidayModel');
 const factory = require('../factoryController');
 
-const asyncError = require('../../utils/asyncError');
-
 exports.getAllHolidays = factory.getAll(Holiday);
-
-exports.addHoliday = asyncError(async (req, res, next) => {
-  const holiday = new Holiday({
-    year: req.body.year,
-    holidays: req.body.holidays
-  });
-
-  const newHoliday = await holiday.save();
-
-  res.json(newHoliday);
-});
+exports.getOneFiascalYearHolidays = factory.getOne(Holiday);
+exports.addNewFiscalYearHolidays = factory.createOne(Holiday);
+exports.updateFiscalYearHolidays = factory.updateOne(Holiday);
+exports.removeSingleHolidayYear = factory.deleteOne(Holiday);
--- routes/holidays/holidayRoutes.js
+++ routes/holidays/holidayRoutes.js
@@ -1,14 +1,24 @@
 const express = require('express');
 
 const fiscalYearMiddleware = require('../../middlewares/fiscalYearMiddleware');
+const authMiddleware = require('../../middlewares/authMiddleware');
 
 const holidayController = require('../../controllers/holidays/holidaysController');
 
 const router = express.Router();
 
 router
+  .route('/:id')
+  .get(
+    fiscalYearMiddleware.getFiscalYear,
+    holidayController.getOneFiascalYearHolidays
+  )
+  .patch(authMiddleware.protect, holidayController.updateFiscalYearHolidays)
+  .delete(authMiddleware.protect, holidayController.removeSingleHolidayYear);
+
+router
   .route('/')
   .get(fiscalYearMiddleware.getFiscalYear, holidayController.getAllHolidays)
-  .post(holidayController.addHoliday);
+  .post(authMiddleware.protect, holidayController.addNewFiscalYearHolidays);
 
 module.exports = router;
