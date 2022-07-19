9c560ad34773c3e0d67c9d703065a81c5cce8dba
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-07-19 10:00:49 +0545

    comments add in fiscal year middleware

--- middlewares/fiscalYearMiddleware.js
+++ middlewares/fiscalYearMiddleware.js
@@ -13,6 +13,7 @@ exports.getFiscalYear = asyncError(async (req, res, next) => {
   let currentFiscalYearStartDate = new Date(`${currentFullYear}-07-15`);
   let currentFiscalYearEndDate = new Date(`${currentFullYear + 1}-07-14`);
 
+  // check if fiscal year start date and end date lies in weekends
   if (new Date(currentFiscalYearStartDate).getDay() === 6) {
     const updatedStartDate = new Date(currentFiscalYearStartDate);
     currentFiscalYearStartDate = updatedStartDate.setDate(
@@ -41,35 +42,32 @@ exports.getFiscalYear = asyncError(async (req, res, next) => {
     );
   }
 
+  // get latest fiscal year holiday
   const latestHolidayYear = await Holiday.findOne().sort({ createdAt: -1 });
 
-  if (
-    new Date(latestHolidayYear.year).getFullYear() ===
-    new Date(currentFiscalYearStartDate).getFullYear()
-  ) {
-    const holidaysList = latestHolidayYear.holidays.map((holiday) =>
-      holiday.date.toString()
+  const holidaysList = latestHolidayYear.holidays.map((holiday) =>
+    holiday.date.toString()
+  );
+
+  // check if fiscal year start date and end date lies in holidays
+  if (holidaysList.includes(new Date(currentFiscalYearStartDate).toString())) {
+    currentFiscalYearStartDate = new Date(currentFiscalYearStartDate).setDate(
+      new Date(currentFiscalYearStartDate).getDate() + 1
     );
+  }
 
-    if (
-      holidaysList.includes(new Date(currentFiscalYearStartDate).toString())
-    ) {
-      currentFiscalYearStartDate = new Date(currentFiscalYearStartDate).setDate(
-        new Date(currentFiscalYearStartDate).getDate() + 1
-      );
-    }
-
-    if (holidaysList.includes(new Date(currentFiscalYearEndDate).toString())) {
-      currentFiscalYearEndDate = new Date(currentFiscalYearEndDate).setDate(
-        new Date(currentFiscalYearEndDate).getDate() - 1
-      );
-    }
+  if (holidaysList.includes(new Date(currentFiscalYearEndDate).toString())) {
+    currentFiscalYearEndDate = new Date(currentFiscalYearEndDate).setDate(
+      new Date(currentFiscalYearEndDate).getDate() - 1
+    );
   }
 
   req.fiscalYear = {
     currentFiscalYearStartDate: new Date(currentFiscalYearStartDate),
     currentFiscalYearEndDate: new Date(currentFiscalYearEndDate)
   };
+
+  console.log(req.fiscalYear);
 
   next();
 });
