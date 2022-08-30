18d560c4f98f521afe56547dbd78f196a1831ace
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-08-30 12:04:47 +0545

    get weekly notice controller added in noticesd

--- controllers/notices/noticeController.js
+++ controllers/notices/noticeController.js
@@ -1,8 +1,31 @@
 const Notice = require('../../models/notices/noticeModel');
 const factory = require('../factoryController');
+const asyncError = require('../../utils/asyncError');
 
 exports.getNotice = factory.getOne(Notice);
 exports.getAllNotices = factory.getAll(Notice);
 exports.createNotice = factory.createOne(Notice);
 exports.updateNotice = factory.updateOne(Notice);
 exports.deleteNotice = factory.deleteOne(Notice);
+
+exports.getWeekNotices = asyncError(async (req, res, next) => {
+  const { todayDate, afterOneWeekDate } = req;
+
+  const notices = await Notice.find({
+    $and: [
+      {
+        startDate: { $gte: todayDate }
+      },
+      {
+        startDate: { $lte: afterOneWeekDate }
+      }
+    ]
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      notices: notices
+    }
+  });
+});
