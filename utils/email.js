f34b419871faf605e3a566a2c4d420ea70a9b5bc
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-11-01 13:50:17 +0545

    fixed email message body

--- utils/email.js
+++ utils/email.js
@@ -5,7 +5,6 @@ class EmailNotification {
     this.transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: true,
       auth: {
         user: process.env.EMAIL_USERNAME,
         pass: process.env.EMAIL_PASSWORD
