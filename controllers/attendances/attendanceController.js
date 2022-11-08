af1fc99c5232ef90e2c2b9224832e1fadc5f2294
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-11-08 16:05:20 +0545

    forgot password message refactored

--- controllers/attendances/attendanceController.js
+++ controllers/attendances/attendanceController.js
@@ -265,17 +265,26 @@ exports.leaveCutForLateAttendace = asyncError(async (req, res, next) => {
 
   const emailContent = await Email.findOne({ module: 'late-attendance' });
 
-  const message = `<b><em>${leaveCutUser.name}</em> late arival leave cut at ${
-    req.body.leaveCutdate.split('T')[0]
-  }</b>`;
+  const message = `<b><em>${
+    leaveCutUser.name
+  }</em> late arival leave cut at ${req.body.leaveCutdate
+    .toString()
+    .split(',')
+    .map((x) => `<p>${x.split('T')[0]}</p>`)
+    .join('')}</b>`;
 
   new EmailNotification().sendEmail({
     email: [INFOWENEMAIL, HRWENEMAIL, leaveCutUser.email],
     subject: emailContent.title || `late arrival leave cut`,
     message:
-      emailContent.body
-        .replace(/@username/i, leaveCutUser.name)
-        .replace(/@date/i, req.body.leaveCutdate) || message
+      emailContent.body.replace(/@username/i, leaveCutUser.name).replace(
+        /@date/i,
+        req.body.leaveCutdate
+          .toString()
+          .split(',')
+          .map((x) => `<p>${x.split('T')[0]}</p>`)
+          .join('')
+      ) || message
   });
 
   res.status(200).json({
