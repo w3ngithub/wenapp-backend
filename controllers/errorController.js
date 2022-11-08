762e7fcc4a6130e1ebf8ab9b2f8755489331a908
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-11-08 14:29:44 +0545

    error controller duplicate value message refactored

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -8,7 +8,7 @@ const handleCastErrorDB = (err) => {
 const handleDuplicateFieldsDB = (err) => {
   const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
 
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  const message = `${value} Already exists: Please use another value!`;
   return new AppError(message, 400);
 };
 
