b93d3af4e9ce0f9b562f3350e8431b08d5085356
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2022-11-18 14:39:56 +0545

    maintenance spelling corrected

--- routes/projects/projectRoutes.js
+++ routes/projects/projectRoutes.js
@@ -5,7 +5,7 @@ const timeLogRouter = require('../timelogs/timeLogRoutes');
 const authMiddleware = require('../../middlewares/authMiddleware');
 
 const router = express.Router();
-router.get('/maintenance', projectController.projectmaintenanceReminder);
+router.get('/maintenance', projectController.projectMaintenanceReminder);
 
 router.use('/:projectId/timelogs', timeLogRouter);
 
