be3e51a461d6c75ccef7fa53a3719d71e52b21b3
ashok22ganika <ganika.ashok@webexpertsnepal.com>
2023-03-13 10:20:01 +0545

    timelog encrypt

--- utils/crypto.js
+++ utils/crypto.js
@@ -26,6 +26,10 @@ const PROJECT_TYPE_KEY = 'projecttypekey123456789';
 const LOG_TYPE_KEY = 'logtypekey123456789';
 const LOG_KEY = 'logkey123456789';
 
+const NOTICE_TYPE_KEY = 'noticetypekey123456789';
+const BLOG_CATEGORY_KEY = 'blogcategorykey123456789';
+const EMAIL_KEY = 'emailkey123456789';
+
 module.exports = {
   encrypt,
   LATE_ARRIVAL_KEY,
@@ -42,5 +46,8 @@ module.exports = {
   USER_POSITION_TYPE_KEY,
   USER_ROLE_KEY,
   LOG_KEY,
-  LOG_TYPE_KEY
+  LOG_TYPE_KEY,
+  NOTICE_TYPE_KEY,
+  BLOG_CATEGORY_KEY,
+  EMAIL_KEY
 };
